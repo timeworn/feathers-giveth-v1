48cfbaaf408766bfe1e1f0ec98d390a7c41c6b67
timeworn <nikolay.voronin93@gmail.com>
2018-03-15 18:35:50 -0700

    fix bug where only 10 reviewers are being returned

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,7 +1,6 @@
 // Application hooks that run for every service
-import { restrictToAuthenticated } from 'feathers-authentication-hooks';
-import auth from 'feathers-authentication';
 import logger from './hooks/logger';
+import { restrictToAuthenticated } from 'feathers-authentication-hooks';
 
 const excludableRestrictToAuthenticated = (...servicesToExclude) => context => {
   if (servicesToExclude.indexOf(context.path) > -1) return context;
@@ -9,22 +8,15 @@ const excludableRestrictToAuthenticated = (...servicesToExclude) => context => {
   return restrictToAuthenticated()(context);
 };
 
-const authenticate = () => context => {
-  // socket connection is already authenticated
-  if (context.params.provider !== 'rest') return context;
-
-  return auth.hooks.authenticate('jwt')(context);
-};
-
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [authenticate(), excludableRestrictToAuthenticated('authentication')],
-    update: [authenticate(), restrictToAuthenticated()],
-    patch: [authenticate(), restrictToAuthenticated()],
-    remove: [authenticate(), excludableRestrictToAuthenticated('authentication')],
+    create: [excludableRestrictToAuthenticated('authentication')],
+    update: [restrictToAuthenticated()],
+    patch: [restrictToAuthenticated()],
+    remove: [excludableRestrictToAuthenticated('authentication')],
   },
 
   after: {
