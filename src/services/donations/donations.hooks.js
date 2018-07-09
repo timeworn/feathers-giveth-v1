1f12838bc9009930f819f70fc5e6264705427275
timeworn <nikolay.voronin93@gmail.com>
2018-07-09 19:20:18 +0200

    refactor Pledges.js

--- src/services/donations/donations.hooks.js
+++ src/services/donations/donations.hooks.js
@@ -195,7 +195,7 @@ const joinDonationRecipient = (item, context) => {
     )
     .then(
       context =>
-        Number(item.intendedProject) > 0
+        item.intendedProject > 0
           ? commons.populate({
               schema: poSchemas[`po-${item.intendedProjectType.toLowerCase()}-intended`],
             })(context)
--- src/utils/processingQueue.js
+++ src/utils/processingQueue.js
@@ -2,6 +2,9 @@ const logger = require('winston');
 const queue = require('./queue');
 const processor = require('./processor');
 
+/**
+ * Factory function for a queue that tracks which id's are currently being processed
+ */
 const processingQueue = target => {
   const q = queue(target);
   const p = processor({});
@@ -20,13 +23,11 @@ const processingQueue = target => {
   });
 };
 
-// cache queues by name
-const queues = {};
-
-function getQueue(name) {
-  if (queues[name]) return queues[name];
-
-  const q = processingQueue({ name });
+/**
+ * create a new ProcessingQueue
+ */
+const factory = name => {
+  const q = processingQueue({});
 
   // for debugging purposes. check if there are any stuck txs every 5 mins
   setInterval(() => {
@@ -34,12 +35,7 @@ function getQueue(name) {
       logger.info(`current "${name}" QUEUE status ->`, JSON.stringify(q.get(), null, 2));
     }
   }, 1000 * 60 * 5);
-
-  queues[name] = q;
   return q;
-}
-
-module.exports = {
-  getQueue,
-  processingQueue,
 };
+
+module.exports = factory;
