08bd88ce8ba022ba271b53f91187a185f5967a6a
timeworn <nikolay.voronin93@gmail.com>
2020-09-04 08:49:55 +0000

    Bump decompress from 4.2.0 to 4.2.1

--- scripts/utils/getPledgeAdmin.js
+++ scripts/utils/getPledgeAdmin.js
@@ -1,17 +1,31 @@
+const Web3 = require('web3');
 const { LiquidPledging } = require('giveth-liquidpledging');
 
-const configFileName = 'test'; // default or beta
+const configFileName = 'default'; // default or beta
 
 // eslint-disable-next-line import/no-dynamic-require
 const config = require(`../../config/${configFileName}.json`);
 
-const { liquidPledgingAddress } = config.blockchain;
+const { nodeUrl, liquidPledgingAddress } = config.blockchain;
 
-const { getWeb3 } = require('../../src/blockchain/lib/web3Helpers');
+const instantiateWeb3 = url => {
+  const provider =
+    url && url.startsWith('ws')
+      ? new Web3.providers.WebsocketProvider(url, {
+          clientConfig: {
+            maxReceivedFrameSize: 100000000,
+            maxReceivedMessageSize: 100000000,
+          },
+        })
+      : url;
+  return new Web3(provider);
+};
 
-const foreignWeb3 = getWeb3({
-  get: key => config[key],
-});
+const foreignWeb3 = instantiateWeb3(
+  nodeUrl.startsWith('ws')
+    ? nodeUrl.replace('wss://', 'http://').replace('ws://', 'http://')
+    : nodeUrl,
+);
 
 /**
   Utility method to get a single pledge from liquidPledging
@@ -21,13 +35,9 @@ const foreignWeb3 = getWeb3({
 
 async function getPledgeAdmin(adminId) {
   const liquidPledging = new LiquidPledging(foreignWeb3, liquidPledgingAddress);
-  const block = await foreignWeb3.eth.getBlock('3050500');
-
-  console.log(block.timestamp);
 
   const admin = await liquidPledging.getPledgeAdmin(adminId);
   console.log('admin', admin);
 }
-setTimeout(() => {
-  getPledgeAdmin(process.argv[2]);
-}, 0);
+
+getPledgeAdmin(process.argv[2]);
