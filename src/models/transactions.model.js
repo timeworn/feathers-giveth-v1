704f08a8d0c6d25707110195e8bd597cf678407c
timeworn <nikolay.voronin93@gmail.com>
2020-12-26 05:56:01 +0330

    Added timestamp to transactions.model.js

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -4,8 +4,10 @@ function Transaction(app) {
   const transaction = new Schema(
     {
       hash: { type: String, required: true, index: true },
-      from: { type: String, required: true },
-      isHome: { type: Boolean, default: false },
+      from: { type: String },
+      blockNumber: { type: Number },
+      isHome: { type: Boolean },
+      timestamp: { type: Date },
     },
     {
       timestamps: false,
