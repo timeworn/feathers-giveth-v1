831329b28222255d49f8188112a271fae1ab36d5
timeworn <nikolay.voronin93@gmail.com>
2021-01-17 16:03:03 +0330

    Remove useless match in aggregateDonation.service

--- src/utils/sendPledgeNotifications.js
+++ src/utils/sendPledgeNotifications.js
@@ -23,6 +23,9 @@ const sendNotification = async (app, pledge) => {
     }
     return app.service('users').get(id);
   };
+
+  await app.service('milestones').get(pledge.delegateTypeId || pledge.ownerTypeId);
+
   const pledgeAdmin = await getAdmin(
     pledge.delegateType || pledge.ownerType,
     pledge.delegateTypeId || pledge.ownerTypeId,
