2209abe390365ab09808b73d97ef7ee5a8e71094
timeworn <nikolay.voronin93@gmail.com>
2021-02-09 00:43:11 +0330

    Milestone propose and donate/delegate actionRecipientAddress are changed.

--- src/services/users/user.service.test.js
+++ src/services/users/user.service.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const config = require('config');
 const { assert } = require('chai');
-const { getJwt, SAMPLE_DATA} = require('../../../test/testUtility');
+const { getJwt, SAMPLE_DATA } = require('../../../test/testUtility');
 const { getFeatherAppInstance } = require('../../app');
 
 const app = getFeatherAppInstance();
@@ -9,13 +9,13 @@ const baseUrl = config.get('givethFathersBaseUrl');
 const relativeUrl = '/users';
 
 function getUserTestCases() {
-  it('should get successful result', async () => {
+  it('should get successful result', async function() {
     const response = await request(baseUrl).get(relativeUrl);
     assert.equal(response.statusCode, 200);
     assert.exists(response.body.data);
     assert.notEqual(response.body.data.length, 0);
   });
-  it('getUserDetail', async () => {
+  it('getUserDetail', async function() {
     const response = await request(baseUrl).get(`${relativeUrl}/${SAMPLE_DATA.USER_ADDRESS}`);
     assert.equal(response.statusCode, 200);
     assert.equal(response.body.address, SAMPLE_DATA.USER_ADDRESS);
@@ -26,7 +26,7 @@ function postUserTestCases() {
   // cant test create use successfully because create user need token and for verifying token
   // you need to have user is like egg and hen
 
-  it('should get unAuthorized error', async () => {
+  it('should get unAuthorized error', async function() {
     const response = await request(baseUrl)
       .post(relativeUrl)
       .send(SAMPLE_DATA.CREATE_MILESTONE_DATA);
@@ -36,7 +36,7 @@ function postUserTestCases() {
 }
 
 function patchUserTestCases() {
-  it('should update name of user successfully', async () => {
+  it('should update name of user successfully', async function() {
     const testName = `testName ${new Date()}`;
     const response = await request(baseUrl)
       .patch(`${relativeUrl}/${SAMPLE_DATA.USER_ADDRESS}`)
@@ -46,67 +46,15 @@ function patchUserTestCases() {
     assert.equal(response.body.name, testName);
   });
 
-  it('should get unAuthorized error', async () => {
+  it('should get unAuthorized error', async function() {
     const response = await request(baseUrl).patch(`${relativeUrl}/${SAMPLE_DATA.USER_ADDRESS}`);
     assert.equal(response.statusCode, 401);
     assert.equal(response.body.code, 401);
   });
-
-  it('should update users access by admin', async () => {
-    const response = await request(baseUrl)
-      .patch(`${relativeUrl}/${SAMPLE_DATA.SECOND_USER_ADDRESS}`)
-      .send({
-        isInReviewerWhitelist: true,
-        isInProjectWhitelist: true,
-        isInDelegateWhitelist: true,
-      })
-      .set({ Authorization: getJwt(SAMPLE_DATA.ADMIN_USER_ADDRESS) });
-    assert.equal(response.statusCode, 200);
-    assert.isTrue(response.body.isInReviewerWhitelist);
-    assert.isTrue(response.body.isInProjectWhitelist);
-    assert.isTrue(response.body.isInDelegateWhitelist);
-
-    console.log('change back secondUsers permissions');
-    await request(baseUrl)
-      .patch(`${relativeUrl}/${SAMPLE_DATA.SECOND_USER_ADDRESS}`)
-      .send({
-        isInReviewerWhitelist: false,
-        isInProjectWhitelist: false,
-        isInDelegateWhitelist: false,
-      })
-      .set({ Authorization: getJwt(SAMPLE_DATA.ADMIN_USER_ADDRESS) });
-  });
-
-  it('non-admin user cant update his/her accesses', async () => {
-    const response = await request(baseUrl)
-      .patch(`${relativeUrl}/${SAMPLE_DATA.SECOND_USER_ADDRESS}`)
-      .send({
-        isInReviewerWhitelist: true,
-        isInProjectWhitelist: true,
-        isInDelegateWhitelist: true,
-      })
-      .set({ Authorization: getJwt(SAMPLE_DATA.SECOND_USER_ADDRESS) });
-    assert.equal(response.statusCode, 403);
-  });
-
-  it('admin user can update his/her accesses', async () => {
-    const response = await request(baseUrl)
-      .patch(`${relativeUrl}/${SAMPLE_DATA.ADMIN_USER_ADDRESS}`)
-      .send({
-        isInReviewerWhitelist: true,
-        isInProjectWhitelist: true,
-        isInDelegateWhitelist: true,
-      })
-      .set({ Authorization: getJwt(SAMPLE_DATA.ADMIN_USER_ADDRESS) });
-    assert.equal(response.statusCode, 200);
-    assert.isTrue(response.body.isInReviewerWhitelist);
-    assert.isTrue(response.body.isInProjectWhitelist);
-    assert.isTrue(response.body.isInDelegateWhitelist);
-  });
 }
 
 function deleteUserTestCases() {
-  it('should get 405, method is no allowed ', async () => {
+  it('should get 405, method is no allowed ', async function() {
     const response = await request(baseUrl)
       .delete(`${relativeUrl}/${SAMPLE_DATA.USER_ADDRESS}`)
       .set({ Authorization: getJwt() });
