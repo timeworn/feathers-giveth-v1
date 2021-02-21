5eebc3ecc66ddf72545b3adafdbe1f9f683bb690
timeworn <nikolay.voronin93@gmail.com>
2021-02-21 20:21:45 +0330

    Update CI-CD.yml

--- src/models/donations.model.js
+++ src/models/donations.model.js
@@ -15,13 +15,6 @@ const DonationStatus = {
   FAILED: 'Failed',
 };
 
-const DONATION_BRIDGE_STATUS = {
-  CANCELLED: 'Cancelled',
-  EXPIRED: 'Expired',
-  UNKNOWN: 'Unknown',
-  PAID: 'Paid',
-};
-
 function Donation(app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
@@ -47,16 +40,11 @@ function Donation(app) {
       campaignId: { type: String },
       status: {
         type: String,
-        required: true,
+        require: true,
         enum: Object.values(DonationStatus),
         default: DonationStatus.PENDING,
         index: true,
       },
-      bridgeStatus: {
-        type: String,
-        enum: Object.values(DONATION_BRIDGE_STATUS),
-      },
-      bridgeTxHash: { type: String },
       txHash: { type: String, index: true },
       homeTxHash: { type: String },
       commitTime: { type: Date },
@@ -138,6 +126,5 @@ function Donation(app) {
 
 module.exports = {
   DonationStatus,
-  DONATION_BRIDGE_STATUS,
   createModel: Donation,
 };
