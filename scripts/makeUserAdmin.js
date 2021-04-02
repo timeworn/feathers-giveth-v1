da735ec4c229155198aceb4abacc8d7e0fb8ba2b
timeworn <nikolay.voronin93@gmail.com>
2021-04-02 14:12:54 +0430

    Get userAddress from process.argv instead of environment variables

---
+++ scripts/makeUserAdmin.js
@@ -0,0 +1,34 @@
+/**
+ * You can run this script like this:
+ * NODE_ENV=develop  node scripts/makeUserAdmin.js 0x5AC583Feb2b1f288C0A51d6Cdca2e8c814BFE93B
+ */
+
+const config = require('config');
+const mongoose = require('mongoose');
+
+const userAddress = process.argv[2];
+
+if (!userAddress) {
+  throw new Error('USER_ADDRESS is required and should pass it');
+}
+const mongoUrl = config.mongodb;
+mongoose.connect(mongoUrl);
+const db = mongoose.connection;
+db.on('error', err => console.error(`Could not connect to Mongo:\n${err.stack}`));
+
+db.once('open', async () => {
+  console.info('Connected to Mongo');
+  const isUserExists = await db.collection('users').findOne({ address: userAddress });
+  if (!isUserExists) {
+    console.error(`User with address ${userAddress} not found`);
+    process.exit(0);
+  }
+  await db.collection('users').updateOne(
+    { address: userAddress },
+    {
+      $set: { isAdmin: true },
+    },
+  );
+  console.log('user updated successfully');
+  process.exit(0);
+});
