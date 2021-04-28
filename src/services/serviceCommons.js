68e1a3f69b74c62660fbb465a34014a48428d1b1
timeworn <nikolay.voronin93@gmail.com>
2021-04-28 20:49:49 +0430

    Rename log title of handleMilestoneConversationAndEmail(), prevent misleading

--- src/services/serviceCommons.js
+++ src/services/serviceCommons.js
@@ -1,18 +1,6 @@
 const defaultFeatherMongooseOptions = {
   multi: ['patch'],
-  whitelist: [
-    '$exists',
-    '$and',
-    '$or',
-    '$not',
-    '$size',
-    '$elemMatch',
-    '$regex',
-    '$options',
-    '$text',
-    '$search',
-    '$meta',
-  ],
+  whitelist: ['$exists', '$and', '$or', '$not', '$size', '$elemMatch', '$regex', '$options'],
 };
 
 module.exports = {
