a8e2fc1af8408af341ff00dfe6724bcb5a664096
timeworn <nikolay.voronin93@gmail.com>
2021-06-15 22:11:21 +0430

    Add sample log for donations event

--- src/services/traces/traces.hooks.js
+++ src/services/traces/traces.hooks.js
@@ -23,14 +23,6 @@ const checkTraceName = require('./checkTraceName');
 const { getBlockTimestamp, ZERO_ADDRESS } = require('../../blockchain/lib/web3Helpers');
 const { getTokenByAddress } = require('../../utils/tokenHelper');
 const createModelSlug = require('../createModelSlug');
-const { isRequestInternal } = require('../../utils/feathersUtils');
-
-const removeProtectedFields = () => context => {
-  if (context && context.data && !isRequestInternal(context)) {
-    delete context.data.verified;
-  }
-  return context;
-};
 
 const traceResolvers = {
   before: context => {
@@ -335,7 +327,6 @@ module.exports = {
     ],
     get: [],
     create: [
-      removeProtectedFields(),
       checkConversionRates(),
       checkTraceDates(),
       checkTraceName(),
@@ -348,7 +339,6 @@ module.exports = {
       createModelSlug('traces'),
     ],
     update: [
-      removeProtectedFields(),
       restrict(),
       checkTraceDates(),
       ...address,
@@ -357,7 +347,6 @@ module.exports = {
       checkTraceName(),
     ],
     patch: [
-      removeProtectedFields(),
       restrict(),
       sanitizeAddress(
         ['pluginAddress', 'reviewerAddress', 'campaignReviewerAddress', 'recipientAddress'],
